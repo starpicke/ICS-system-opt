fdb829f4bf62a7eef357bde839cfd562303d4cc0
starpicke <3032863135@qq.com>
2025-11-22 20:58:16 +0800

    首次提交，完成了波特率计算页面

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    QFile file(":/style/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    qApp->setStyleSheet(styleSheet);
+    return a.exec();
+}
